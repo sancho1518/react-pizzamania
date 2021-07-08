088213b68ff6dc752e12f6243b96ec5b718a824d
Sadiqwahab <sanchosadiq@gmail.com>
2021-07-08 15:47:18 +0000

    add Carousel.jsx

---
+++ src/Carousel.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Carousel extends Component {
+    render() {
+        return (
+            <div>
+                Carousel
+            </div>
+        );
+    }
+}
+
+export default Carousel;
