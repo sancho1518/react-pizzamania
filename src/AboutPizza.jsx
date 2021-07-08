d1dbafe2abf399280d808b921b168e52fe121d5a
Sadiqwahab <sanchosadiq@gmail.com>
2021-07-08 15:47:40 +0000

    add AboutPizza.jsx

---
+++ src/AboutPizza.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AboutPizza extends Component {
+    render() {
+        return (
+            <div>
+                AboutPizza
+            </div>
+        );
+    }
+}
+
+export default AboutPizza;
+
